712e9876bcd61a2a2db279b273d0f6030cc21ffe
Bianca-Leal <bleal8882@gmail.com>
2024-03-29 17:47:42 -0300

    Adicionando atributos mago

--- src/Mago.java
+++ src/Mago.java
@@ -1,3 +1,28 @@
-public class Mago extends Jogador{
+
+public class Mago extends Jogador {
 	private int inteligencia;
+	private int magia;
+
+	public Mago(int vida, int ataque, int inteligencia, int magia) {
+		super(vida, ataque);
+		this.inteligencia = inteligencia;
+		this.magia = magia;
+	}
+
+	public int getInteligencia() {
+		return inteligencia;
+	}
+
+	public void setInteligencia(int inteligencia) {
+		this.inteligencia = inteligencia;
+	}
+
+	public int getMagia() {
+		return magia;
+	}
+
+	public void setMagia(int magia) {
+		this.magia = magia;
+	}
+
 }
