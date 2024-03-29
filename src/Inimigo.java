53647805380580b68f975808e7e30efcfdecaa6f
danibotelho <danibl26@gmail.com>
2024-03-29 15:57:18 -0300

    Criando metodos da classe inimigo

--- src/Inimigo.java
+++ src/Inimigo.java
@@ -1,11 +1,15 @@
+import java.util.Random;
+
 public class Inimigo {
 
     private int vida;
     private int ataque;
+    private int defesa;
 
-    public Inimigo(int vida, int ataque) {
-        this.vida = vida;
-        this.ataque = ataque;
+    public Inimigo() {
+        this.vida = 30;
+        this.ataque = gerarDefesaAleatoria();
+        this.defesa = gerarDefesaAleatoria();
     }
 
     public int getVida() {
@@ -22,5 +26,37 @@ public class Inimigo {
 
     public void setAtaque(int ataque) {
         this.ataque = ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    private int gerarDefesaAleatoria() {
+        Random random = new Random();
+        return random.nextInt(20) + 1;
+    }
+
+    public void atacar(Jogador jogador) {
+        int vidaJogador = jogador.getVida();
+        int vidaAtualizadaJogador = vidaJogador - ataque;
+
+        if (vidaAtualizadaJogador < 0) {
+            vidaAtualizadaJogador = 0;
+        }
+        jogador.setVida(vidaAtualizadaJogador);
+    }
+
+
+    public void estaVivo() {
+        if (vida > 0) {
+            System.out.println("O inimigo está vivo. Vida restante:  " + vida);
+        } else {
+            System.out.println("Inimigo está morto");
+        }
     }
 }
